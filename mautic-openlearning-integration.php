79234840348295644e94b95ca3b4962e0db9f7ef
Davi Gabriel da Silva <davi.gbr@gmail.com>
2017-10-13 18:13:51 -0300

    Update mautic-openlearning-integration.php

--- mautic-openlearning-integration.php
+++ mautic-openlearning-integration.php
@@ -1,5 +1,5 @@
 <?php
-// mautic.targetteal.com/mautic-openlearning-integration.php
+// targetteal.com/mautic-openlearning-integration.php
 
 $formId = $_POST['formId'];
 $formName = $_POST['formName'];
@@ -7,7 +7,7 @@ $email = $_POST['email'];
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"http://www.example.com/tester.phtml");
+curl_setopt($ch, CURLOPT_URL, "https://mautic.targetteal.com/form/submit?formId=$formId");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
 	"mauticform[formId]" => $formId,
